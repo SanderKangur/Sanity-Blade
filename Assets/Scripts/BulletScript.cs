381372aa9897300443e1346c0ebca4d87b9fecfa
Bogdan <bogdanmihhailjuk@gmail.com>
2019-10-14 14:34:29 +0300

    setup

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+
+    public float speed;
+    Rigidbody2D rb;
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        moveDirection.z = 0;
+        moveDirection.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + moveDirection * speed * Time.deltaTime;
+        Destroy(gameObject, 3f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float velX;
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public float health;
+    bool facingRight = true;
+
+    public GameObject projectile;
+    Vector2 projectilePos;
+    public float fireRate;
+    float nextFire = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velX = Input.GetAxisRaw("Horizontal");
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Shoot();
+        }
+       
+    }
+    void LateUpdate()
+    {
+        Vector3 localScale = transform.localScale;
+        if (velX > 0)
+        {
+            facingRight = true;
+        }
+        else if (velX < 0)
+        {
+            facingRight = false;
+        }
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+        {
+            localScale.x *= -1;
+        }
+        transform.localScale = localScale;
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
+    }
+
+    void Shoot()
+    {
+        projectilePos = transform.position;
+        Instantiate(projectile, projectilePos, Quaternion.identity);
+    }
+
+  
+}
